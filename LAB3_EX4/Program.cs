62cb1984cec6685e38d834421ba0372b7204cbe7
uimkatali <b8bf6b5@gmail.com>
2023-10-02 18:42:51 +0300

    Add project files.

---
+++ LAB3_EX4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace lab3ex4
+{
+
+    /*
+     Un program care va normaliza un sir de caractere citit de la tastatura astfel incat
+    fiecare cuvant din sir sa inceapa cu MAJUSCULA, restul literelor fiind
+    transformate in litere mici.
+    Exemplu:
+    Input: Ana ARE mErE
+    Rezultat: Ana Are Mere
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sir = Console.ReadLine();
+            TextInfo text = new CultureInfo("en-US", false).TextInfo;
+            Console.WriteLine(text.ToTitleCase(sir));
+        }
+    }
+}
